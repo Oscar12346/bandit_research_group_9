5e06422a58ed1f0e77c40c3d8864dbc4f936ce51
tomasosarte <tomasosarte@gmail.com>
2025-05-26 01:24:00 +0200

    Add minor fix

--- src/adversaries/adversary.py
+++ src/adversaries/adversary.py
@@ -6,7 +6,12 @@ class Adversary:
 
     def get_reward(self, action: int, context: np.ndarray) -> float:
         pass
+
     def get_best_reward(self):
         pass
+
+    def get_mean_rewards(self, context: np.ndarray, action: int) -> np.ndarray:
+        pass
+    
     def reset(self):
         pass
