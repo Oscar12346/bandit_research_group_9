d37ba6adeec6b9fb1d36e2346919068a4bf92cfb
tomasosarte <tomasosarte@gmail.com>
2025-06-20 16:34:23 +0200

    fixed experiment

--- src/display/visualization.py
+++ src/display/visualization.py
@@ -5,33 +5,44 @@ class Visualization:
 
     @staticmethod
     def plot_mean_std(
-        data: np.ndarray, 
+        data: list[np.ndarray],
+        data_labels: list[str] = None,
         title: str = "", 
         xlabel: str = "",
         ylabel: str = "",
         baseline: np.ndarray = None,
         baseline_title: str = "Baseline"
     ):
-        mean_values = np.mean(data, axis=0)
-        std_values = np.std(data, axis=0)
-        timesteps = np.arange(data.shape[1])
+        
+        if len(data) != len(data_labels):
+            raise ValueError("data and data_labels must have the same length")
+        
+        # Get mean and std for each dataset
+        mean_values = []
+        std_values = []
+        for i in range(len(data)):
+            mean_values.append(np.mean(data[i], axis=0))
+            std_values.append(np.std(data[i], axis=0))
 
+        # Get the number of timesteps
+        timesteps = np.arange(data[0].shape[1])
+
+        # Plot each dataset with its mean and std
         plt.figure(figsize=(10, 6))
-        plt.plot(timesteps, mean_values, label='Mean')
-        plt.fill_between(
-            timesteps, 
-            mean_values - std_values, 
-            mean_values + std_values,
-            color='b', 
-            alpha=0.2, 
-            label='±1 Std. Dev.'
-        )
+        for i in range(len(data)):
+            plt.plot(timesteps, mean_values[i], label=data_labels[i])
+            plt.fill_between(
+                timesteps, 
+                mean_values[i] - std_values[i], 
+                mean_values[i] + std_values[i],
+                alpha=0.2
+            )
 
         # Plot baseline if provided
         if baseline is not None:
             baseline = baseline.flatten()  # ensure 1D
-            if baseline.shape[0] != data.shape[1]:
-                raise ValueError(f"Baseline length {baseline.shape[0]} does not match data horizon {data.shape[1]}")
+            if baseline.shape[0] != data[0].shape[1]:
+                raise ValueError(f"Baseline length {baseline.shape[0]} does not match data horizon {data[0].shape[1]}")
             plt.plot(timesteps, baseline, label=baseline_title, color='r', linestyle='--')
 
         plt.xlabel(xlabel)
